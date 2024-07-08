3001402f1ba3eb99067b1a25ddf1d61e791bd23b
Mirella Barros <mirella.b@aluno.ifsc.edu.br>
2024-07-08 11:11:36 -0300

    Correções de bugs.

--- libprg/src/libprg/alg_ord_troca.c
+++ libprg/src/libprg/alg_ord_troca.c
@@ -3,7 +3,7 @@
 void bubblesort(int* vetor, int tamanho, bool decrescente) {
 
 	for (int i = 0; i < tamanho; i++) {
-		for (int j = 0; j < (tamanho - i - 1); j--) {
+		for (int j = 0; j < (tamanho - i - 1); j++) {
 			if (decrescente) {
 				if (vetor[j] > vetor[j + 1]) {
 					int aux = vetor[j];
@@ -22,7 +22,7 @@ void bubblesort(int* vetor, int tamanho, bool decrescente) {
 }
 
 void insertion(int* vetor, int tamanho) {
-	for (int i = 0; i < (tamanho - 1); ++i) {
+	for (int i = 0; i < (tamanho); ++i) {
 		int chave = vetor[i];
 		int j = i - 1;
 		while (j >= 0 && vetor[j] > chave) {
